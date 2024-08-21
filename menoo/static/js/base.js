cdd2946b6d5b1b08ed90924b650a93fd763fea02
Gabriel Traiano <gabriel.traiano4@gmail.com>
2024-08-20 23:13:39 -0300

    Finished the side-menu

--- menoo/static/js/base.js
+++ menoo/static/js/base.js
@@ -1,10 +1,19 @@
 let sideButton = document.querySelector('.side-button')
 let sideMenu = document.querySelector('.side-menu')
 
-sideButton.onclick = () =>{
-    menu_side();
-}
-
 function menu_side () {
     sideMenu.classList.toggle('active-side')
 }
+
+sideButton.onclick = (event) => {
+    if (!event.target.closest('.side-menu')){
+        sideMenu.classList.add('active-side')
+    }
+}
+
+window.addEventListener('click', (event) => {
+    // Verifica se o clique foi fora do menu e do botão
+    if (!sideMenu.contains(event.target) && !sideButton.contains(event.target)) {
+        sideMenu.classList.remove('active-side');
+    }
+});
